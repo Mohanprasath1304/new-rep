f20dd27787df308db4f24afc10a35158bfd6957a
Mohanprasath1304 <srmprasath1340@gmail.com>
2024-09-13 20:27:43 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -39,6 +39,12 @@ class calculator {
         result = number1 / number2;
         System.out.println(number1 + " / " + number2 + " = " + result);
         break;
+        
+      case '/':
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        
+        break;
 
       default:
         System.out.println("Invalid operator!");
